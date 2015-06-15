598c4d0c44e2fb54e95f244095399dc79b7aafd9
eunseok.jeong <esjeong80@gmail.com>
2015-06-15 10:40:08 +0900

    printf 구문 반복 수행

--- test.c
+++ test.c
@@ -3,7 +3,10 @@
 int main()
 {
 
-	printf("Hello World~!\n");
+	printf("Hello World1~!\n");
+	printf("Hello World2~!\n");
+	printf("Hello World3~!\n");
+	printf("Hello World4~!\n");
 
 	return 0;
 }
